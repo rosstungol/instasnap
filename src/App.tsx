3af3f78cff31749de81ebcba2d1a3e39efd2592c
Ross Tungol <ross@proudcloud.io>
2023-12-12 23:35:33 +0800

    Adds Shadcn Toaster components

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SigninForm from "./_auth/forms/SigninForm"
 import SignupForm from "./_auth/forms/SignupForm"
 import { Home } from "./_root/pages"
 import RootLayout from "./_root/RootLayout"
+import { Toaster } from "@/components/ui/toaster"
 
 const App = () => {
   return (
@@ -18,6 +19,8 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+
+      <Toaster />
     </main>
   )
 }
