07e3f56e0360adf079bfc665d198abb4acb656f0
Ross Tungol <ross@proudcloud.io>
2023-12-21 23:50:05 +0800

    Updates Topbar component content and styles

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,5 +1,50 @@
+import { useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { Button } from "../ui/button"
+import { useUserContext } from "@/context/AuthContext"
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations"
+
 const Topbar = () => {
-  return <div>Topbar</div>
+  const { mutate: signOut, isSuccess } = useSignOutAccount()
+  const navigate = useNavigate()
+  const { user } = useUserContext()
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0)
+    }
+  }, [isSuccess])
+
+  return (
+    <div className='topbar'>
+      <div className='flex-between py-4 px-5'>
+        <Link to='/' className='flex gap-3 items-center'>
+          <img
+            src='/assets/images/logo.svg'
+            alt='logo'
+            width={130}
+            height={325}
+          />
+        </Link>
+        <div className='flex gap-4'>
+          <Button
+            variant='ghost'
+            className='shad-button_ghost'
+            onClick={() => signOut()}
+          >
+            <img src='/assets/icons/logout.svg' />
+          </Button>
+          <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
+            <img
+              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              alt='profile'
+              className='h-8 w-8 rounded-full'
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Topbar
