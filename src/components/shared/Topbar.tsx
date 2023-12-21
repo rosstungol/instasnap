650f46f219300f21e31602ad47c6fd30cbae266b
Ross Tungol <ross@proudcloud.io>
2023-12-22 00:21:13 +0800

    Adds alt alttribute to logout icon in Topbar component

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -32,7 +32,7 @@ const Topbar = () => {
             className='shad-button_ghost'
             onClick={() => signOut()}
           >
-            <img src='/assets/icons/logout.svg' />
+            <img src='/assets/icons/logout.svg' alt='logout' />
           </Button>
           <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
             <img
