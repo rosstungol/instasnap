9da6fa08452154d9480e4c6fc062e986877ca31b
Ross Tungol <ross@proudcloud.io>
2023-12-26 02:17:34 +0800

    Adds Routes for page components

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,18 @@ import "./globals.css"
 import AuthLayout from "./_auth/AuthLayout"
 import SigninForm from "./_auth/forms/SigninForm"
 import SignupForm from "./_auth/forms/SignupForm"
-import { Home } from "./_root/pages"
+import {
+  Home,
+  Explore,
+  Saved,
+  Profile,
+  UpdateProfile,
+  AllUsers,
+  CreatePost,
+  EditPost,
+  PostDetails,
+  LikedPosts
+} from "./_root/pages"
 import RootLayout from "./_root/RootLayout"
 import { Toaster } from "@/components/ui/toaster"
 
@@ -17,6 +28,15 @@ const App = () => {
         </Route>
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/saved' element={<Saved />} />
+          <Route path='/profile/:id/*' element={<Profile />} />
+          <Route path='/update-profile/:id' element={<UpdateProfile />} />
+          <Route path='/all-users' element={<AllUsers />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:id' element={<EditPost />} />
+          <Route path='/posts/:id' element={<PostDetails />} />
+          <Route path='/liked-posts' element={<LikedPosts />} />
         </Route>
       </Routes>
 
