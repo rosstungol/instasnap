d0bac787d9fac8436803dcb82133bb927f1a89e3
Ross Tungol <ross@proudcloud.io>
2024-01-11 23:51:01 +0800

    Implements post like functionality

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -25,18 +25,38 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
 
   const { data: currentUser } = useGetCurrentUser()
 
+  const handleLikePost = (e: React.MouseEvent) => {
+    e.stopPropagation()
+
+    let newLikes = [...likes]
+    const hasLiked = newLikes.includes(userId)
+
+    if (hasLiked) {
+      newLikes = newLikes.filter((id) => id !== userId)
+    } else {
+      newLikes.push(userId)
+    }
+
+    setLikes(newLikes)
+    likePost({ postId: post.$id, likesArray: newLikes })
+  }
+
   return (
     <div className='flex justify-between items-center z-20'>
       <div className='flex gap-2 mr-5'>
         <img
-          src='/assets/icons/like.svg'
+          src={
+            checkIsLiked(likes, userId)
+              ? "/assets/icons/liked.svg"
+              : "/assets/icons/like.svg"
+          }
           alt='like'
           width={20}
           height={20}
-          onClick={() => {}}
+          onClick={handleLikePost}
           className='cursor-pointer'
         />
-        <p className='small-medium lg:base-medium'>0</p>
+        <p className='small-medium lg:base-medium'>{likes.length}</p>
       </div>
       <div className='flex gap-2'>
         <img
