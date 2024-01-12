7b705bbb5d167f5b2f7838dd32ca14f1ec40eed3
Ross Tungol <ross@proudcloud.io>
2024-01-13 03:39:38 +0800

    Adds updatePost function to api.ts

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,5 +1,5 @@
 import { ID, Query } from "appwrite"
-import { INewPost, INewUser } from "@/types"
+import { INewPost, INewUser, IUpdatePost } from "@/types"
 import { appwriteConfig, account, databases, avatars, storage } from "./config"
 
 export async function createUserAccount(user: INewUser) {
@@ -247,6 +247,64 @@ export async function getPostById(postId: string) {
     )
 
     return post
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function updatePost(post: IUpdatePost) {
+  const hasFileToUpdate = post.file.length > 0
+
+  try {
+    let image = {
+      imageUrl: post.imageUrl,
+      imageId: post.imageId
+    }
+
+    if (hasFileToUpdate) {
+      // Upload image to storage
+      const uploadedFile = await uploadFile(post.file[0])
+
+      if (!uploadedFile) throw Error
+
+      // Get file url
+      const fileUrl = getFilePreview(uploadedFile.$id)
+
+      if (!fileUrl) {
+        deleteFile(uploadedFile.$id)
+        throw Error
+      }
+
+      image = {
+        ...image,
+        imageUrl: fileUrl,
+        imageId: uploadedFile.$id
+      }
+    }
+
+    // Concert tags into an array
+    const tags = post.tags?.replace(/ /g, "").split(",") || []
+
+    // Save post to database
+    const updatedPost = await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      post.postId,
+      {
+        caption: post.caption,
+        imageUrl: image.imageUrl,
+        imageId: image.imageId,
+        location: post.location,
+        tags: tags
+      }
+    )
+
+    if (!updatedPost) {
+      await deleteFile(post.imageId)
+      throw Error
+    }
+
+    return updatedPost
   } catch (error) {
     console.log(error)
   }
