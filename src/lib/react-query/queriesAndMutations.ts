5d7d317a65ddb9ea8fac6725e1c9e36acd63c0ff
Ross Tungol <ross@proudcloud.io>
2024-01-13 03:52:22 +0800

    Adds updatePost mutation function

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -14,9 +14,10 @@ import {
   likePost,
   savePost,
   signInAccount,
-  signOutAccount
+  signOutAccount,
+  updatePost
 } from "../appwrite/api"
-import { INewPost, INewUser } from "@/types"
+import { INewPost, INewUser, IUpdatePost } from "@/types"
 import { QUERY_KEYS } from "./queryKeys"
 
 export const useCreateUserAccount = () => {
@@ -137,5 +138,18 @@ export const useGetPostById = (postId: string) => {
     queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
     queryFn: () => getPostById(postId),
     enabled: !!postId
+  })
+}
+
+export const useUpdatePost = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (post: IUpdatePost) => updatePost(post),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id]
+      })
+    }
   })
 }
