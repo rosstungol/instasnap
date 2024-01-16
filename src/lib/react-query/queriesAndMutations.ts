45c0b76cacb3030e3ebbb313901fbc17a07698e8
Ross Tungol <ross@proudcloud.io>
2024-01-16 23:15:09 +0800

    Adds useGetPosts query function

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -10,6 +10,7 @@ import {
   deletePost,
   deleteSavedPost,
   getCurrentUser,
+  getInfinitePosts,
   getPostById,
   getRecentPosts,
   likePost,
@@ -165,6 +166,20 @@ export const useDeletePost = () => {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
       })
+    }
+  })
+}
+
+export const useGetPosts = () => {
+  return useInfiniteQuery({
+    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+    queryFn: getInfinitePosts,
+    getNextPageParam: (lastPage) => {
+      if (lastPage && lastPage.documents.length === 0) return null
+
+      const lastId = lastPage.documents[lastPage?.documents.length - 1].$id
+
+      return lastId
     }
   })
 }
