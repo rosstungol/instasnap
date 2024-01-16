2bb57dae0c2d7f3a06f00843d4a650df2aada2c7
Ross Tungol <ross@proudcloud.io>
2024-01-17 02:05:35 +0800

    Updates GridPostList component content and styles

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,5 +1,29 @@
-const GridPostList = () => {
-  return <div>GridPostList</div>
+import { Link } from "react-router-dom"
+import { useUserContext } from "@/context/AuthContext"
+import { Models } from "appwrite"
+
+type GridPostListProps = {
+  posts: Models.Document[]
+}
+
+const GridPostList = ({ posts }: GridPostListProps) => {
+  const { user } = useUserContext()
+
+  return (
+    <ul className='grid-container'>
+      {posts.map((post) => (
+        <li key={post.$id} className='relative min-w-80 h-80'>
+          <Link to={`/posts/${post.$id}`} className='grid-post_link'>
+            <img
+              src={post.imageUrl}
+              alt='post'
+              className='h-full w-full object-cover'
+            />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default GridPostList
