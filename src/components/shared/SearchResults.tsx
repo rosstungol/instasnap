d212c760e976e51ac6434fdae51b6ed3cd312e36
Ross Tungol <ross@proudcloud.io>
2024-01-17 03:31:45 +0800

    Implements SearchResults functionality

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,5 +1,23 @@
-const SearchResults = () => {
-  return <div>SearchResults</div>
+import { Models } from "appwrite"
+import GridPostList from "./GridPostList"
+import Loader from "./Loader"
+
+type SearchResultsProps = {
+  isSearchFetching: boolean
+  searchedPosts: Models.Document[]
+}
+
+const SearchResults = ({
+  isSearchFetching,
+  searchedPosts
+}: SearchResultsProps) => {
+  if (isSearchFetching) return <Loader />
+
+  if (searchedPosts && searchedPosts.documents.length > 0) {
+    return <GridPostList posts={searchedPosts.documents} />
+  }
+
+  return <p className='text-light-4 mt-10 text-center w-full'>SearchResults</p>
 }
 
 export default SearchResults
