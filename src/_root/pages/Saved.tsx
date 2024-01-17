78bb764671767b6a8296909b5ef95f6556b50c2f
Ross Tungol <ross@proudcloud.io>
2024-01-17 23:47:40 +0800

    Implements Saved page UI

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,5 +1,50 @@
+import { NavLink, useLocation } from "react-router-dom"
+import { savedPostLinks } from "@/constants"
+
 const Saved = () => {
-  return <div>Saved</div>
+  const { pathname } = useLocation()
+
+  return (
+    <div className='flex flex-1'>
+      <div className='saved-container'>
+        <div className='max-w-5xl flex-start gap-3 justify-start w-full'>
+          <img src='/assets/icons/save.svg' width={36} height={36} alt='add' />
+          <h2 className='h3-bold md:h2-bold text-left w-full'>Saved Posts</h2>
+        </div>
+
+        <div className='flex-between w-full max-w-5xl mb-7'>
+          <ul className='saved-links_list'>
+            {savedPostLinks.map((link) => {
+              const isActive = pathname === link.route
+
+              return (
+                <li key={link.label} className='saved-link_item'>
+                  <NavLink
+                    to={link.route}
+                    className={`saved-link ${isActive && "bg-dark-3"}`}
+                  >
+                    <img src={link.imgURL} />
+                    {link.label}
+                  </NavLink>
+                </li>
+              )
+            })}
+          </ul>
+          <div className='flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer'>
+            <p className='small-medium md:base-medium text-light-2'>All</p>
+            <img
+              src='/assets/icons/filter.svg'
+              width={20}
+              height={20}
+              alt='filter'
+            />
+          </div>
+        </div>
+
+        <div className='flex flex-wrap gap-9 w-full max-w-5xl'></div>
+      </div>
+    </div>
+  )
 }
 
 export default Saved
