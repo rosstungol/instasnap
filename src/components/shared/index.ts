e362de81e8173ad934816f0c888c24622cd04d8f
Ross Tungol <ross@proudcloud.io>
2024-01-19 02:57:51 +0800

    Fixes typo in import

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,7 +1,7 @@
 export { default as Bottombar } from "./Bottombar"
 export { default as FileUploader } from "./FileUploader"
 export { default as GridPostList } from "./GridPostList"
-export { default as LeftSideBar } from "./LeftSidebar"
+export { default as LeftSidebar } from "./LeftSidebar"
 export { default as Loader } from "./Loader"
 export { default as PostCard } from "./PostCard"
 export { default as PostStats } from "./PostStats"
