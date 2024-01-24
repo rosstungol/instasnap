a3653635f15d22ac1b8507eaf4a883111e24c9c7
Ross Tungol <ross@proudcloud.io>
2024-01-24 19:15:54 +0800

    Adds Loader to PostStats like button

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -20,7 +20,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const [likes, setLikes] = useState(likesList)
   const [isSaved, setIsSaved] = useState(false)
 
-  const { mutate: likePost } = useLikePost()
+  const { mutate: likePost, isPending: isLikingPost } = useLikePost()
   const { mutate: savePost, isPending: isSavingPost } = useSavePost()
   const { mutate: deleteSavedPost, isPending: isDeletingSaved } =
     useDeleteSavedPost()
@@ -66,18 +66,23 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   return (
     <div className='flex justify-between items-center z-20'>
       <div className='flex gap-2 mr-5'>
-        <img
-          src={
-            checkIsLiked(likes, userId)
-              ? "/assets/icons/liked.svg"
-              : "/assets/icons/like.svg"
-          }
-          alt='like'
-          width={20}
-          height={20}
-          onClick={handleLikePost}
-          className='cursor-pointer'
-        />
+        {isLikingPost ? (
+          <Loader />
+        ) : (
+          <img
+            src={
+              checkIsLiked(likes, userId)
+                ? "/assets/icons/liked.svg"
+                : "/assets/icons/like.svg"
+            }
+            alt='like'
+            width={20}
+            height={20}
+            onClick={handleLikePost}
+            className='cursor-pointer'
+          />
+        )}
+
         <p className='small-medium lg:base-medium'>{likes.length}</p>
       </div>
       <div className='flex gap-2'>
