596bfce83fee78e978772fae0dfc9f1a77aad306
Ross Tungol <ross@proudcloud.io>
2024-01-26 02:20:49 +0800

    Adds ProfileUploader component

--- src/components/shared/ProfileUploader.tsx
+++ src/components/shared/ProfileUploader.tsx
@@ -1,5 +1,47 @@
-const ProfileUploader = () => {
-  return <div>ProfileUploader</div>
+import { useCallback, useState } from "react"
+import { useDropzone, FileWithPath } from "react-dropzone"
+import { convertFileToUrl } from "@/lib/utils"
+
+type ProfileUploaderProps = {
+  fieldChange: (files: File[]) => void
+  mediaUrl: string
+}
+
+const ProfileUploader = ({ fieldChange, mediaUrl }: ProfileUploaderProps) => {
+  const [file, setFile] = useState<File[]>([])
+  const [fileUrl, setFileUrl] = useState(mediaUrl)
+
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFile(acceptedFiles)
+      fieldChange(acceptedFiles)
+      setFileUrl(convertFileToUrl(acceptedFiles[0]))
+    },
+    [file]
+  )
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".png", "jpeg", ".jpg", ".svg"]
+    }
+  })
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <div className='flex items-center gap-4 cursor-pointer'>
+        <img
+          src={fileUrl || "/assets/icons/profile-placeholder.svg"}
+          alt='image'
+          className='w-24 h-24 rounded-full object-cover object-top'
+        />
+        <p className='text-primary-500 small-regular md:base-semibold'>
+          Change profile photo
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default ProfileUploader
