a26ff6b917ebed345cfb954c2778ddbd2bfd9257
Ross Tungol <ross@proudcloud.io>
2024-01-26 02:19:43 +0800

    Adds ProfileForm component

---
+++ src/components/forms/ProfileForm.tsx
@@ -0,0 +1,146 @@
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { ProfileValidation } from "@/lib/validation"
+import { Textarea } from "../ui"
+import PhotoUploader from "../shared/ProfileUploader"
+import { useUserContext } from "@/context/AuthContext"
+import { useParams } from "react-router-dom"
+import { useGetUserById } from "@/lib/react-query/queries"
+import { Loader } from "../shared"
+
+const ProfileForm = () => {
+  const { id } = useParams()
+  const { data: currentUser } = useGetUserById(id || "")
+  const { user } = useUserContext()
+
+  const form = useForm<z.infer<typeof ProfileValidation>>({
+    resolver: zodResolver(ProfileValidation),
+    defaultValues: {
+      file: [],
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      bio: user.bio || ""
+    }
+  })
+
+  if (!currentUser)
+    return (
+      <div className='flex-center w-full h-full'>
+        <Loader />
+      </div>
+    )
+
+  function onSubmit(values: z.infer<typeof ProfileValidation>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex flex-col gap-9 w-full max-w-5xl'
+      >
+        <FormField
+          control={form.control}
+          name='file'
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <PhotoUploader
+                  fieldChange={field.onChange}
+                  mediaUrl={currentUser.imageUrl}
+                />
+              </FormControl>
+              <FormMessage className='shad-form_message' />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='shad-form_label'>Name</FormLabel>
+              <FormControl>
+                <Input className='shad-input' {...field} />
+              </FormControl>
+              <FormMessage className='shad-form_message' />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='shad-form_label'>Username</FormLabel>
+              <FormControl>
+                <Input className='shad-input' {...field} />
+              </FormControl>
+              <FormMessage className='shad-form_message' />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='shad-form_label'>Email</FormLabel>
+              <FormControl>
+                <Input type='email' className='shad-input' {...field} />
+              </FormControl>
+              <FormMessage className='shad-form_message' />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='bio'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='shad-form_label'>Bio</FormLabel>
+              <FormControl>
+                <Textarea
+                  className='shad-textarea custom-scrollbar'
+                  {...field}
+                />
+              </FormControl>
+
+              <FormMessage className='shad-form_message' />
+            </FormItem>
+          )}
+        />
+
+        <div className='flex gap-4 items-center justify-end'>
+          <Button type='button' className='shad-button_dark_4'>
+            Cancel
+          </Button>
+          <Button
+            type='submit'
+            className='shad-button_primary whitespace-nowrap'
+          >
+            Update Profile
+          </Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
+
+export default ProfileForm
