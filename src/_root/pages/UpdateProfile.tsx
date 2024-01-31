9054aa54e029db69838b54cf58ec5d98ca2a6d3f
Ross Tungol <ross@proudcloud.io>
2024-01-31 22:01:43 +0800

    Removes unnecessary div in UpdateProfile component

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -2,21 +2,19 @@ import { ProfileForm } from "@/components/forms"
 
 const UpdateProfile = () => {
   return (
-    <div className='flex flex-1'>
-      <div className='common-container'>
-        <div className='max-w-5xl flex-start gap-3 justify-start w-full'>
-          <img
-            src='/assets/icons/edit.svg'
-            width={36}
-            height={36}
-            alt='add'
-            className='invert-white'
-          />
-          <h2 className='h3-bold md:h2-bold text-left w-full'>Edit Profile</h2>
-        </div>
-
-        <ProfileForm />
+    <div className='common-container'>
+      <div className='max-w-5xl flex-start gap-3 justify-start w-full'>
+        <img
+          src='/assets/icons/edit.svg'
+          width={36}
+          height={36}
+          alt='add'
+          className='invert-white'
+        />
+        <h2 className='h3-bold md:h2-bold text-left w-full'>Edit Profile</h2>
       </div>
+
+      <ProfileForm />
     </div>
   )
 }
