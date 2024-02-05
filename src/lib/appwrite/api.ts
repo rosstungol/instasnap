e16509d4f2667306b43c62000f59095b318ef78f
Ross Tungol <ross@proudcloud.io>
2024-02-05 23:58:52 +0800

    Adds optional parameter to getPostById function

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -242,13 +242,17 @@ export async function deleteSavedPost(savedRecordId: string) {
   }
 }
 
-export async function getPostById(postId: string) {
+export async function getPostById(postId?: string) {
+  if (!postId) throw Error
+
   try {
     const post = await databases.getDocument(
       appwriteConfig.databaseId,
       appwriteConfig.postCollectionId,
       postId
     )
+
+    if (!post) throw Error
 
     return post
   } catch (error) {
