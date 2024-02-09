d9eedc5d7d85fd59bf72574b419a69e86221792e
Ross Tungol <ross@proudcloud.io>
2024-02-09 19:37:23 +0800

    Adds followers and following collection id to appwrite config

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -7,7 +7,9 @@ export const appwriteConfig = {
   storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
   userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
   postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
-  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+  followingCollectionId: import.meta.env.VITE_APPWRITE_FOLLOWING_COLLECTION_ID,
+  followersCollectionId: import.meta.env.VITE_APPWRITE_FOLLOWERS_COLLECTION_ID
 }
 
 export const client = new Client()
