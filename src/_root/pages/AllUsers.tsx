c8035b5b75e42e617ff62206de3166ac052b021b
Ross Tungol <ross@proudcloud.io>
2024-02-27 21:32:00 +0800

    Implements infinite scrolling to AllUsers component

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,15 +1,60 @@
+import { useEffect } from "react"
+import { useInView } from "react-intersection-observer"
 import { Loader } from "@/components/shared"
 import UserCard from "@/components/shared/UserCard"
 import { toast } from "@/components/ui"
-import { useGetUsers } from "@/lib/react-query/queries"
+import { useGetAllUsers } from "@/lib/react-query/queries"
 
 const AllUsers = () => {
-  const { data: creators, isPending, isError: isErrorCreators } = useGetUsers()
+  const { ref, inView } = useInView()
+
+  const {
+    data,
+    fetchNextPage,
+    isFetchingNextPage,
+    isError: isErrorCreators
+  } = useGetAllUsers()
+
+  useEffect(() => {
+    if (inView) fetchNextPage()
+  }, [inView])
+
+  const creators = data?.pages.flatMap((page) => page)
+
+  if (!creators) {
+    return (
+      <div className='common-container'>
+        <Loader />
+      </div>
+    )
+  }
 
   if (isErrorCreators) {
     toast({ title: "Something went wrong." })
     return
   }
+
+  const userGrid = (
+    <ul className='user-grid'>
+      {creators.map((creator, i) => {
+        if (i + 1 === creators.length)
+          return (
+            <li
+              ref={ref}
+              key={creator?.$id}
+              className='flex-1 min-w-[200px] w-full'
+            >
+              <UserCard user={creator!} />
+            </li>
+          )
+        return (
+          <li key={creator?.$id} className='flex-1 min-w-[200px] w-full'>
+            <UserCard user={creator!} />
+          </li>
+        )
+      })}
+    </ul>
+  )
 
   return (
     <div className='common-container'>
@@ -24,16 +69,11 @@ const AllUsers = () => {
         <h2 className='h3-bold md:h2-bold text-left w-full'>All Users</h2>
       </div>
 
-      {isPending && !creators ? (
-        <Loader />
-      ) : (
-        <ul className='user-grid'>
-          {creators?.map((creator) => (
-            <li key={creator.$id} className='flex-1 min-w-[200px] w-full'>
-              <UserCard user={creator} />
-            </li>
-          ))}
-        </ul>
+      {userGrid}
+      {isFetchingNextPage && (
+        <div className='mt-10'>
+          <Loader />
+        </div>
       )}
     </div>
   )
