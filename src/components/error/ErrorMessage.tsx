a4844443bad878260853bf772313e37530b8d840
Ross Tungol <ross@proudcloud.io>
2024-02-29 18:56:44 +0800

    Adds ErrorMessage component

---
+++ src/components/error/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+const ErrorMessage = () => {
+  return (
+    <div className='flex-center p-8'>
+      <div className='flex-center border border-dark-4 rounded-[20px] p-8'>
+        <p>Something went wrong. Please refresh the page.</p>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorMessage
