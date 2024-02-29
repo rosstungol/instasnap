1b702b699419a638c0074bbc2ef68296d0d609ce
Ross Tungol <ross@proudcloud.io>
2024-02-29 18:58:06 +0800

    Adds error handling to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import AuthProvider from "./context/AuthContext"
 import { QueryProvider } from "./lib/react-query/QueryProvider"
+import { ErrorBoundary } from "./components/error"
+import { ErrorMessage } from "./components/error"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <QueryProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </QueryProvider>
-  </BrowserRouter>
+  <ErrorBoundary fallback={<ErrorMessage />}>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </ErrorBoundary>
 )
