1ade36544ad470fb75db23334e8fbe6286c8f00f
Ross Tungol <ross@proudcloud.io>
2024-02-29 18:56:20 +0800

    Adds ErrorBoundary component

---
+++ src/components/error/ErrorBoundary.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from "react"
+
+type ErrorBoundaryProps = {
+  fallback: ReactNode
+  children: ReactNode
+}
+
+type ErrorBoundaryState = {
+  hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.error(error, info)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback
+    }
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
