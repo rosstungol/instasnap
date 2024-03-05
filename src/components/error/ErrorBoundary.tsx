b1058a31c6da2f92e8af00eeec664895d6823efd
Ross Tungol <ross@proudcloud.io>
2024-03-05 19:53:22 +0800

    Removes unused variable

--- src/components/error/ErrorBoundary.tsx
+++ src/components/error/ErrorBoundary.tsx
@@ -18,7 +18,7 @@ class ErrorBoundary extends React.Component<
     this.state = { hasError: false }
   }
 
-  static getDerivedStateFromError(error: Error) {
+  static getDerivedStateFromError() {
     return { hasError: true }
   }
 
