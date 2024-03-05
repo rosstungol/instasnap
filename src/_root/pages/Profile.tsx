21a52e29417540e9e2a01653dbb97059535310f8
Ross Tungol <ross@proudcloud.io>
2024-03-05 19:48:34 +0800

    Moves StatBlock component to a new file

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -9,25 +9,13 @@ import {
 } from "react-router-dom"
 import { useInView } from "react-intersection-observer"
 import { LikedPosts } from "."
-import { GridPostItem, Loader } from "@/components/shared"
+import { GridPostItem, Loader, StatBlock } from "@/components/shared"
 import { FollowButton } from "@/components/shared"
 import { useUserContext } from "@/context/AuthContext"
 import {
   useGetUserById,
   useGetUserProfilePosts
 } from "@/lib/react-query/queries"
-
-interface StabBlockProps {
-  value: string | number
-  label: string
-}
-
-const StatBlock = ({ value, label }: StabBlockProps) => (
-  <div className='flex-center gap-2'>
-    <p className='small-semibold lg:body-bold text-primary-500'>{value}</p>
-    <p className='small-medium lg:base-medium text-light-2'>{label}</p>
-  </div>
-)
 
 const Profile = () => {
   const { ref, inView } = useInView()
@@ -79,6 +67,7 @@ const Profile = () => {
       })}
     </ul>
   )
+
   return (
     <div className='profile-container'>
       <div className='profile-inner_container'>
@@ -98,7 +87,6 @@ const Profile = () => {
                 @{profileUser.username}
               </p>
             </div>
-
             <div className='flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20'>
               <StatBlock value={profileUser.posts.length} label='Posts' />
               <StatBlock
@@ -110,12 +98,10 @@ const Profile = () => {
                 label='Following'
               />
             </div>
-
             <p className='small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm'>
               {profileUser.bio}
             </p>
           </div>
-
           <div className='flex justify-center gap-4'>
             {user.id === profileUser.$id ? (
               <Link
@@ -188,6 +174,7 @@ const Profile = () => {
         <Route path='/liked-posts' element={<LikedPosts />} />
       </Routes>
       <Outlet />
+
       {isFetchingNextPage && (
         <div className='mt-10'>
           <Loader />
