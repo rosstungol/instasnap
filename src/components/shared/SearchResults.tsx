1f6f4165f930309bec43c0e42ee7e9d34de41ddf
Ross Tungol <ross@proudcloud.io>
2024-03-05 19:43:49 +0800

    Updates SearchResults component

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -4,7 +4,7 @@ import Loader from "./Loader"
 
 type SearchResultsProps = {
   isSearchFetching: boolean
-  searchedPosts: Models.Document[]
+  searchedPosts?: Models.Document[]
 }
 
 const SearchResults = ({
@@ -13,11 +13,11 @@ const SearchResults = ({
 }: SearchResultsProps) => {
   if (isSearchFetching) return <Loader />
 
-  if (searchedPosts && searchedPosts.documents.length > 0) {
-    return <GridPostList posts={searchedPosts.documents} />
+  if (searchedPosts && searchedPosts.length > 0) {
+    return <GridPostList posts={searchedPosts} />
   }
 
-  return <p className='text-light-4 mt-10 text-center w-full'>SearchResults</p>
+  return <p className='text-light-4 mt-10 text-center w-full'>No posts found</p>
 }
 
 export default SearchResults
