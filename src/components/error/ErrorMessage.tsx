3c26443314ac2bb6ea3b51b5ea6b2fbf63c551fd
Ross Tungol <ross@proudcloud.io>
2024-03-06 21:52:23 +0800

    Updates ErrorMessage component

--- src/components/error/ErrorMessage.tsx
+++ src/components/error/ErrorMessage.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "../ui"
+
 const ErrorMessage = () => {
+  const navigate = useNavigate()
+  const redirectToHome = () => {
+    navigate("/")
+    window.location.reload()
+  }
+
   return (
     <div className='flex-center p-8'>
-      <div className='flex-center border border-dark-4 rounded-[20px] p-8'>
-        <p>Something went wrong. Please refresh the page.</p>
+      <div className='flex flex-col border border-dark-4 rounded-[20px] p-8 m-8 gap-4'>
+        <h1 className='body-bold md:h3-bold'>Something went wrong</h1>
+        <Button onClick={redirectToHome} className='shad-button_primary'>
+          Go to Home Feed
+        </Button>
       </div>
     </div>
   )
